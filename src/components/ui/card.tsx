ce5b0d54680cbd383eb8ad69f4f3bf0f3693d3a3
rishabh <rishabhgupta.ks3@gmail.com>
2023-11-07 17:55:57 +0530

    Added card shadow on hover effect | Rishabh

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      'rounded-xl border bg-card text-card-foreground shadow-xl bg-slate-50 py-5 px-3 shadow-[#e11318]/60',
+      'rounded-xl border bg-card text-card-foreground shadow-lg transition ease-in-out duration-300 shadow-[#e11318]/60 hover:shadow-2xl bg-slate-50 py-5 px-3 hover:shadow-[#e11318]/80',
       className
     )}
     {...props}
